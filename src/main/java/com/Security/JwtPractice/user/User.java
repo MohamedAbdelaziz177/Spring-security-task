1e96c5a5483c1e06b3434502c9f7ac030b593517
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-11 00:22:14 +0200

    User class added

---
+++ src/main/java/com/Security/JwtPractice/user/User.java
@@ -0,0 +1,14 @@
+package com.Security.JwtPractice.user;
+
+public class User {
+
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+}
