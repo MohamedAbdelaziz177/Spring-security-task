afef8e1c07095072af5d326fb7712c7572f19b05
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-11 00:25:12 +0200

    adding lomboc annotations / Entitizing the class

--- src/main/java/com/Security/JwtPractice/user/User.java
+++ src/main/java/com/Security/JwtPractice/user/User.java
@@ -1,6 +1,20 @@
 package com.Security.JwtPractice.user;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "user")
 public class User {
+
 
     private Integer id;
 
