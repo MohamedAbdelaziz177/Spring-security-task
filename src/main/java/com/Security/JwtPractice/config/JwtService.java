bbe5b32068b208587a14698ec07edb00e2972554
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-11 02:17:01 +0200

    fully implemented The service layer

---
+++ src/main/java/com/Security/JwtPractice/config/JwtService.java
@@ -0,0 +1,67 @@
+package com.Security.JwtPractice.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private final static String SECRET_KEY = "MySuperStrongSecretKey123@";
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+    public String generateToken(
+            Map<String, Object> claims,
+            UserDetails userDetails
+    )
+    {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) { return (Date) extractClaim(token, Claims::getExpiration);}
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver){
+        return claimResolver.apply(extractAllClaims(token));
+    }
+
+    public Claims extractAllClaims(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Key getSigningKey() {
+
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+
+        return key;
+    }
+
+
+}
