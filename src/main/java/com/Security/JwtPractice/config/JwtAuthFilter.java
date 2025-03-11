5515791b74658ea6ce9573a305de2aacd8bfb7e4
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-11 02:13:16 +0200

    JWT Auth Filter Basic setting

---
+++ src/main/java/com/Security/JwtPractice/config/JwtAuthFilter.java
@@ -0,0 +1,42 @@
+package com.Security.JwtPractice.config;
+
+import jakarta.persistence.Column;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+             @NonNull HttpServletRequest request,
+             @NonNull HttpServletResponse response,
+             @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userName;
+        final String email;
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+
+        userName = jwtService.extractUsername(jwt);
+    }
+}
