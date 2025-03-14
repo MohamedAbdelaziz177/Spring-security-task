c81850c535ad499720b59b810aab27f1e36f47c5
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-14 03:40:40 +0200

    first version of controller

---
+++ src/main/java/Controllers/EmployeeController.java
@@ -0,0 +1,43 @@
+package Controllers;
+
+import jakarta.persistence.Tuple;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+    @GetMapping("/getAll")
+    public List<String> getAll(){
+
+        List<String> list = new ArrayList<>();
+
+        list.add("John");
+        list.add("Jane");
+        list.add("Mary");
+        list.add("Bob");
+
+        return list;
+    }
+
+    @GetMapping("/get")
+    public String getEmployee(){
+        return "John";
+    }
+
+    @GetMapping("/update")
+    public String updateEmployee(){
+        return "this action updates employee";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteEmployee(){
+        return "this action deletes employee";
+    }
+}
