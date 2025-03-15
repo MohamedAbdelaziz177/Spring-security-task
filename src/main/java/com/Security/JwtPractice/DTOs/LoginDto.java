852f812f417875c5bc4a9101ab19a010219f5aab
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-15 02:53:58 +0200

    configuring lombok

--- src/main/java/com/Security/JwtPractice/DTOs/LoginDto.java
+++ src/main/java/com/Security/JwtPractice/DTOs/LoginDto.java
@@ -2,6 +2,13 @@ package com.Security.JwtPractice.DTOs;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class LoginDto {
 
